7cfe6c06f7a40fd3c297e714e3c3c5f3923c63c0
Cristian Ros <cristianros.dev@gmail.com>
2025-04-23 22:35:23 +0200

    Little cleanup on home page

---
+++ src/lib/components/auth/buttons/LogInOutButton.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { authClient } from '$lib/auth-client';
+
+	const session = authClient.useSession();
+</script>
+
+{#if $session.data}
+	<button class="btn-form" onclick={() => authClient.signOut()}> Sign out </button>
+{:else}
+	<button class="btn-form" onclick={async () => await goto('/login')}> Log in </button>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,5 @@
 <script>
-	import { goto } from '$app/navigation';
-	import { authClient } from '$lib/auth-client';
-
-	const session = authClient.useSession();
+	import LogInOutButton from '$lib/components/auth/buttons/LogInOutButton.svelte';
 </script>
 
-{#if $session.data}
-	<button class="btn-form" onclick={async () => await authClient.signOut()}> Sign out </button>
-{:else}
-	<button class="btn-form" onclick={async () => await goto('/login')}> Log in </button>
-{/if}
+<LogInOutButton />
