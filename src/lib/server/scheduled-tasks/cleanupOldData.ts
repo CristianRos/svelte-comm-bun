7370dceab8f14448aaa7dd38931ef34a02afef4f
Cristian Ros <cristianros.dev@gmail.com>
2025-04-29 21:19:45 +0200

    Fix env cloudinary name

--- src/lib/server/scheduled-tasks/cleanupOldData.ts
+++ src/lib/server/scheduled-tasks/cleanupOldData.ts
@@ -6,7 +6,7 @@ import { gte } from "drizzle-orm";
 import 'dotenv/config'
 
 v2.config({
-    cloud_name: process.env.VITE_CLOUDINARY_NAME,
+    cloud_name: process.env.VITE_CLOUDINARY_CLOUD_NAME,
     api_key: process.env.VITE_CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true
