d1a8e1e53f8cca213823e0c5494358518ae66546
Cristian Ros <cristianros.dev@gmail.com>
2025-04-29 21:14:43 +0200

    Changed task to work with dotenv vars

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,12 +1,12 @@
-import { env } from '$env/dynamic/private';
 import * as schema from './schema'
 import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import 'dotenv/config'
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const pool = new Pool({
-    connectionString: env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL
 })
 
 export const db = drizzle(pool, { schema });
--- src/lib/server/utils/cloudinary.ts
+++ src/lib/server/utils/cloudinary.ts
@@ -1,11 +1,12 @@
-import { VITE_CLOUDINARY_CLOUD_NAME, VITE_CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } from "$env/static/private"
+// import { VITE_CLOUDINARY_CLOUD_NAME, VITE_CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } from "$env/static/private"
 import { v2 } from "cloudinary"
+import 'dotenv/config'
 
 export async function deleteUnusedCloudinaryImages(images: string[]){
     v2.config({
-        cloud_name: VITE_CLOUDINARY_CLOUD_NAME,
-        api_key: VITE_CLOUDINARY_API_KEY,
-        api_secret: CLOUDINARY_API_SECRET,
+        cloud_name: process.env.VITE_CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.VITE_CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
         secure: true
     })
     images.forEach((img) => {
